d46a935cd567047dfb2efa93a853110244e29a95
Wallyson Jhonatan de Oliveira <waloli@futura.sbo>
2023-11-13 12:49:49 -0300

    project commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# React + TypeScript + Vite
+
+### 🚀 First project on the journey to learn ReactJS and Typescript
+
+### 🧐 Summary
+• It's a simple project to introduce new knowledge. I'm a dev with experience in the Vue ecosystem and some concepts are very similar, which makes learning easier.  
+• The main focus was to exercise basic knowledge through a list of tasks with crud methods such as: add, update and remove.
+
+### 🛠️ Tech stack
+• This project was created with Vite and used some react hooks in addition to local storage.   
+• Also was used CSS and HTML concepts.
+
+
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+import '../assets/TodoList.css';
+import Icone from '../assets/todoImage.png'
+
+interface TodoItem {
+    text: string;
+    isCompleted: boolean;
+}
+
+function TodoList() {
+    const storageList = localStorage.getItem('list');
+    const [list, setList] = useState<TodoItem[]>(storageList ? JSON.parse(storageList) : []);
+    const [newItem, setNewItem] = useState<string>('');
+    
+    useEffect( () => {
+        localStorage.setItem('list', JSON.stringify(list));
+    }, [list])
+
+    function handleAdd(form: any) {        
+        form.preventDefault();
+            
+        if (!newItem) return; 
+        
+        setList([...list, {text: newItem, isCompleted: false}]);
+        setNewItem('');
+        
+        document.getElementById('input-field')?.focus();
+    }
+
+    function handleComplete(index:number) {
+        const listAux = [...list];
+        listAux[index].isCompleted = !listAux[index].isCompleted;
+        
+        return setList(listAux);
+    }
+
+    function handleDelete(index:number) {
+        const listAux = [...list];
+        listAux.splice(index,1);
+
+        return setList(listAux);
+    }
+
+    function handleDeleteAll() {
+        return setList([]);
+    }
+
+    return (
+        <div>
+            <h1>Lista de Tarefas</h1>
+
+            <form onSubmit={handleAdd}>
+                <input 
+                    id="input-field"
+                    type="text" 
+                    placeholder="Adicione uma tarefa" 
+                    value={newItem}
+                    onChange={ (event) => {
+                        setNewItem(event.target.value)
+                    }}
+                />
+                <button type="submit" className="lnr lnr-plus-circle add" title="Adicionar tarefa"></button>    
+            </form>
+
+            <div className="todoList">
+                <div>
+                    {
+                        list.length < 1 
+                            ? 
+                                <img className="img__icon" src={Icone} alt="Imagem sobre lista de tarefas" /> 
+                            : 
+                                list.map((item, index) => (
+                                    <div
+                                        key={index}
+                                        className={item.isCompleted ? "item item__complete" : "item"}
+                                    >
+                                        <span onClick={() => {handleComplete(index)}}>{item.text}</span>
+                                        <button className="lnr lnr-trash remove" onClick={() => {handleDelete(index)}} title="Remover tarefa"></button>
+                                    </div>
+                                ))         
+                    }     
+                    {
+                        list.length > 0 &&
+                        <button className="remove remove__all"  onClick={() => {handleDeleteAll()}} title="Remover todas as tarefas">Remover todos</button>
+                    }
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default TodoList
